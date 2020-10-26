118d983b193c3a025ef6b8939b7d7a2ee65ae4b9
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-26 10:00:47 -0600

    added gitignore;

---
+++ CSharpXLink/CSharpXLink/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpXLink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
